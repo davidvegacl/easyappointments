0f1000c6776c5477e500480b0501ede3fc48d711
David Vega Regollo <david@izarus.cl>
2021-03-02 23:43:15 -0300

    UPD: configuraciones y htaccess

--- application/config/email.php
+++ application/config/email.php
@@ -1,14 +1,16 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
 
 // Add custom values by settings them to the $config array.
 // Example: $config['smtp_host'] = 'smtp.gmail.com';
 // @link https://codeigniter.com/user_guide/libraries/email.html
 
-$config['useragent'] = 'Easy!Appointments';
-$config['protocol'] = 'mail'; // or 'smtp'
+$config['useragent'] = 'Agendastic';
 $config['mailtype'] = 'html'; // or 'text'
-// $config['smtp_host'] = '';
-// $config['smtp_user'] = '';
-// $config['smtp_pass'] = '';
-// $config['smtp_crypto'] = 'ssl'; // or 'tls'
-// $config['smtp_port'] = 25;
+$config['protocol'] = Config::MAIL_PROTOCOL; // 'mail' or 'smtp'
+$config['smtp_host'] = Config::MAIL_SMTP_HOST;
+$config['smtp_user'] = Config::MAIL_SMTP_USER;
+$config['smtp_pass'] = Config::MAIL_SMTP_PASS;
+$config['smtp_crypto'] = Config::MAIL_SMTP_CRYPTO; // 'ssl' or 'tls'
+$config['smtp_port'] = Config::MAIL_SMTP_PORT; // 25
